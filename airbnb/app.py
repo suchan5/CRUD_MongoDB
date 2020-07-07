3f1f1d99db967821079a089dd598a2d005653d67
suchan5 <suchan5@hotmail.com>
2020-07-07 17:58:57 +0000

    next page &  previous page buttons added

--- airbnb/app.py
+++ airbnb/app.py
@@ -12,9 +12,29 @@ DB_NAME = "sample_airbnb"
 
 client = pymongo.MongoClient(MONGO_URI)
 
+'''
 all_accomodations = client[DB_NAME].listingsAndReviews.find().limit(10)
 for a in all_accomodations:
     print(a)
+
+'''
+
+
+@app.route('/')
+def show_listings():
+    page_number = request.args.get('page')  # get the page number
+
+    if page_number == None:  # if there is no page number(a.k.a None), assume we are at page 0
+        page_number = 0
+    else:
+        page_number = int(page_number)
+    print("page_number=", page_number)
+
+    all_listings = client[DB_NAME].listingsAndReviews.find().skip(page_number*20).limit(20)  # skip()요고를 넣어야지만 next 20 listings 버튼을 눌렀을 때 다음 20개로 점프(?)스킵(?)해서 보여준다.
+    return render_template('show_listings.template.html',
+                           all_listings=all_listings,
+                           page_number=page_number
+                           )
 
 
 # "magic code" -- boilerplate
